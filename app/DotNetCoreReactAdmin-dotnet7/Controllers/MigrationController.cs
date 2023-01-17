672c036fc7156bfe9b2c7b39a0ad010e7ef2c40e
Leonardo Santana <leonardonsantana@gmail.com>
2023-01-16 20:30:11 -0500

    Add Migration

---
+++ app/DotNetCoreReactAdmin-dotnet7/Controllers/MigrationController.cs
@@ -0,0 +1,25 @@
+﻿using DotNetCoreReactAdmin_dotnet7.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNetCoreReactAdmin_dotnet7.Controllers
+{
+    [ApiExplorerSettings(GroupName = "Migrations")]
+    [Route("api/ApplyDatabaseMigrations")]
+    [ApiController]
+    public class MigrationController : ControllerBase
+    {
+        protected readonly DotNetCoreReactAdminContext _context;
+
+        public MigrationController(DotNetCoreReactAdminContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            await _context.Database.MigrateAsync();
+            return Ok();
+        }
+    }
+}
